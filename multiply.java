bf1a3deadf7760375fe6c15f888a838157d74668
Chavi Tomer <chavitomar10sep@gmail.com>
2021-07-29 00:56:46 +0530

    dev - modified

--- multiply.java
+++ multiply.java
@@ -1,1 +1,11 @@
- this is to multiply two numbers
+// this is to multiply two numbers
+
+public class Multipl{
+
+	public static void main(String args[]){
+		int a = 10;
+		int b = 100;
+		
+		System.out.println(a*b);
+	}
+}
